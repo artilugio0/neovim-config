035b52046c2e4dae5b9b9a8aa1860f88d94bf953
artilugio0 <artilugio0@proton.me>
2025-02-08 15:55:19 +0000

    add makeprg for golang

--- after/ftplugin/go.lua
+++ after/ftplugin/go.lua
@@ -5,6 +5,8 @@ vim.opt_local.tabstop = 4
 vim.opt_local.shiftwidth = 4
 vim.opt_local.softtabstop = 4
 
+vim.opt_local.makeprg = 'go build -o /dev/null ./...'
+
 if not pcall(libfmt.set_format_program, 'go', 'goimports') then
   pcall(libfmt.set_format_program, 'go', 'gofmt')
 end
