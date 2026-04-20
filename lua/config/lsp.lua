ecc394b1db8e568c621fe094e170d5fff4c5b157
artilugio0 <artilugio0@proton.me>
2026-04-20 17:04:54 -0300

    change diagnostic keymap behavior

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -15,7 +15,15 @@ vim.api.nvim_create_autocmd('LspAttach', {
 
     -- diagnostics
     vim.keymap.set("n", "<leader>ee", vim.diagnostic.open_float, opts)
-    vim.keymap.set("n", "<leader>ep", function() vim.diagnostic.jump({count = -1}) end, opts)
-    vim.keymap.set("n", "<leader>en", function() vim.diagnostic.jump({count = 1}) end, opts)
+
+    vim.keymap.set("n", "<leader>ep", function()
+      vim.diagnostic.jump({count = -1})
+      vim.diagnostic.open_float()
+    end, opts)
+
+    vim.keymap.set("n", "<leader>en", function()
+      vim.diagnostic.jump({count = 1})
+      vim.diagnostic.open_float()
+    end, opts)
   end
 })
